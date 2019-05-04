421378a31c888429a0f54a67625252b0be3b8702
Dušan Maďar <madar.dusan@gmail.com>
2019-05-04 16:52:31 +0200

    Add a bit more precise info to about page

--- ig5_web/app.py
+++ ig5_web/app.py
@@ -30,7 +30,17 @@ def index():
 
 @app.route("/o_ig5.html")
 def about():
-    return render_template("about.html", photos=utils.get_photos("about"))
+    countries = schools["schools"].keys()
+    team_counts = [s["basic"]["teamCount"] for s in summaries.values()]
+    return render_template(
+        "about.html",
+        team_count_min=min(team_counts),
+        team_count_max=max(team_counts),
+        school_count=len(utils.flatten_schools(schools)),
+        country_count=len(countries),
+        countries=countries,
+        photos=utils.get_photos("about"),
+    )
 
 
 @app.route("/<int:order>rocnik.html")
