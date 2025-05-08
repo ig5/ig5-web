9dd21d168d16d3676f89847f5819b1b3780685b8
Dušan Maďar <madar.dusan@gmail.com>
2025-05-08 18:07:21 +0200

    WIP - elevation profile

--- ig5_web/static/js/maps.js
+++ ig5_web/static/js/maps.js
@@ -144,6 +144,334 @@ function renderRouteMap(center, zoom, data) {
   });
   map.addControl(L.control.layers(baseMaps, overlayMaps));
   map.addControl(L.control.scale({ position: "bottomright", imperial: false }));
+
+  // var elevation_options = {
+  //   // theme: "white-theme",
+  //   almostOver: true,
+  //   waypoints: false,
+  //   detached: true,
+  //   position: "bottomright",
+  //   speed: false,
+  //   altitude: true,
+  //   time: false,
+  //   summary: false,
+  //   legend: false,
+  //   distanceMarkers: false,
+  //   distance: true,
+  //   hotline: true,
+  //   downloadLink: false,
+  //   ruler: true,
+  //   summary: false,
+  //   wptIcons: false,
+  //   // height: 200,
+  // };
+  // var controlElevation = L.control.elevation(elevation_options).addTo(map);
+  // var elevationGeoJson = {
+  //   results: [
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 278.52667236328125,
+  //       location: {
+  //         lat: 48.355,
+  //         lng: 19.57547,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 249.78109741210938,
+  //       location: {
+  //         lat: 48.35396,
+  //         lng: 19.57876,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 256.8179626464844,
+  //       location: {
+  //         lat: 48.35567,
+  //         lng: 19.57832,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 239.8062286376953,
+  //       location: {
+  //         lat: 48.35685,
+  //         lng: 19.5797,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 244.0004425048828,
+  //       location: {
+  //         lat: 48.36045,
+  //         lng: 19.57555,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 243.7277069091797,
+  //       location: {
+  //         lat: 48.36301,
+  //         lng: 19.57226,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 248.00827026367188,
+  //       location: {
+  //         lat: 48.36251,
+  //         lng: 19.5696,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 249.32687377929688,
+  //       location: {
+  //         lat: 48.36674,
+  //         lng: 19.5658,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 258.1531066894531,
+  //       location: {
+  //         lat: 48.37156,
+  //         lng: 19.56126,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 265.7034606933594,
+  //       location: {
+  //         lat: 48.37922,
+  //         lng: 19.55347,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 287.6736145019531,
+  //       location: {
+  //         lat: 48.3739,
+  //         lng: 19.54786,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 288.09490966796875,
+  //       location: {
+  //         lat: 48.37265,
+  //         lng: 19.54908,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 341.3575439453125,
+  //       location: {
+  //         lat: 48.37347,
+  //         lng: 19.53913,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 353.8987731933594,
+  //       location: {
+  //         lat: 48.37409,
+  //         lng: 19.53701,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 368.0915832519531,
+  //       location: {
+  //         lat: 48.37496,
+  //         lng: 19.53689,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 394.8926086425781,
+  //       location: {
+  //         lat: 48.37567,
+  //         lng: 19.53568,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 425.6297302246094,
+  //       location: {
+  //         lat: 48.3767,
+  //         lng: 19.53526,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 427.70159912109375,
+  //       location: {
+  //         lat: 48.37792,
+  //         lng: 19.53159,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 436.0555725097656,
+  //       location: {
+  //         lat: 48.37796,
+  //         lng: 19.53073,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 385.57177734375,
+  //       location: {
+  //         lat: 48.37425,
+  //         lng: 19.5347,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 373.0794982910156,
+  //       location: {
+  //         lat: 48.373,
+  //         lng: 19.5358,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 384.9365539550781,
+  //       location: {
+  //         lat: 48.36984,
+  //         lng: 19.5349,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 398.2651062011719,
+  //       location: {
+  //         lat: 48.36927,
+  //         lng: 19.53272,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 376.81427001953125,
+  //       location: {
+  //         lat: 48.36693,
+  //         lng: 19.52875,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 358.52716064453125,
+  //       location: {
+  //         lat: 48.36533,
+  //         lng: 19.52947,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 361.3663024902344,
+  //       location: {
+  //         lat: 48.36476,
+  //         lng: 19.5294,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 374.2415771484375,
+  //       location: {
+  //         lat: 48.36479,
+  //         lng: 19.5288,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 384.4287414550781,
+  //       location: {
+  //         lat: 48.36222,
+  //         lng: 19.52817,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 314.80108642578125,
+  //       location: {
+  //         lat: 48.35776,
+  //         lng: 19.54036,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 292.545166015625,
+  //       location: {
+  //         lat: 48.35779,
+  //         lng: 19.54522,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 297.5272521972656,
+  //       location: {
+  //         lat: 48.35868,
+  //         lng: 19.54606,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 265.1868591308594,
+  //       location: {
+  //         lat: 48.35482,
+  //         lng: 19.56684,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 265.5827331542969,
+  //       location: {
+  //         lat: 48.35299,
+  //         lng: 19.56932,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 265.25152587890625,
+  //       location: {
+  //         lat: 48.35327,
+  //         lng: 19.56962,
+  //       },
+  //     },
+  //     {
+  //       dataset: "eudem25m",
+  //       elevation: 263.963134765625,
+  //       location: {
+  //         lat: 48.35383,
+  //         lng: 19.57046,
+  //       },
+  //     },
+  //   ],
+  //   status: "OK",
+  // };
+
+  // const geojson = {
+  //   name: "demo.geojson",
+  //   type: "FeatureCollection",
+  //   features: [
+  //     {
+  //       type: "Feature",
+  //       geometry: {
+  //         type: "LineString",
+  //         coordinates: elevationGeoJson.results.map((item) => [item.location.lng, item.location.lat, item.elevation]),
+  //       },
+  //     },
+  //   ],
+  // };
+  // // console.log(geojson);
+
+  // // controlElevation.load("/static/js/foo.json");
+  // controlElevation.load(JSON.stringify(geojson));
+  //
+  // https://www.opentopodata.org/api/
+  // https://api.opentopodata.org/v1/eudem25m?locations=48.355,19.57547|48.35396,19.57876|48.35567,19.57832|48.35685,19.5797|48.36045,19.57555|48.36301,19.57226|48.36251,19.5696|48.36674,19.5658|48.37156,19.56126|48.37922,19.55347|48.3739,19.54786|48.37265,19.54908|48.37347,19.53913|48.37409,19.53701|48.37496,19.53689|48.37567,19.53568|48.3767,19.53526|48.37792,19.53159|48.37796,19.53073|48.37425,19.5347|48.373,19.5358|48.36984,19.5349|48.36927,19.53272|48.36693,19.52875|48.36533,19.52947|48.36476,19.5294|48.36479,19.5288|48.36222,19.52817|48.35776,19.54036|48.35779,19.54522|48.35868,19.54606|48.35482,19.56684|48.35299,19.56932|48.35327,19.56962|48.35383,19.57046
 }
 
 function renderSchoolsMap(center, zoom, data) {
